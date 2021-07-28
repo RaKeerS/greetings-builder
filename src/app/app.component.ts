9c5017eb2c75b1a8b70e600a75ddf3bf7238b4c4
RaKeerS <n.srikar22@gmail.com>
2021-07-29 00:17:31 +0530

    feat(prime-ng): doing theme setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { PrimeNGConfig } from 'primeng/api';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'birthday-greetings';
+  constructor(private primengConfig: PrimeNGConfig) {
+
+  }
+
+  ngOnInit() {
+    this.primengConfig.ripple = true;
+  }
 }
