98f77c2e55bf4d6d5d8b46a12a046bc5774f41f2
RaKeerS <n.srikar22@gmail.com>
2021-10-31 02:22:37 +0530

    feat(integrating-data): binding ui with data beta 1.

--- src/app/components/greetings-template/greetings-template.component.ts
+++ src/app/components/greetings-template/greetings-template.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Store } from '@ngxs/store';
 import { SelectTemplate } from 'src/app/actions/birthday-greetings-actions';
 
@@ -11,11 +11,37 @@ export class GreetingsTemplateComponent implements OnInit {
 
   public displayDialog: boolean;
 
+  public responsiveOptions: any;
+
+  @Input('templateInfo') templateInfo: any;
+
   constructor(private store: Store) {
     this.displayDialog = false;
+    this.setResponsiveOptions();
    }
 
   ngOnInit(): void {
+    console.log('templateInfo', this.templateInfo);
+  }
+
+  private setResponsiveOptions() {
+    this.responsiveOptions = [
+      {
+          breakpoint: '1024px',
+          numVisible: 3,
+          numScroll: 3
+      },
+      {
+          breakpoint: '768px',
+          numVisible: 2,
+          numScroll: 2
+      },
+      {
+          breakpoint: '560px',
+          numVisible: 1,
+          numScroll: 1
+      }
+  ];
   }
 
   public get currentTemplateLabel(): string {
