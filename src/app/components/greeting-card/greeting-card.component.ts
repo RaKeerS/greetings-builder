f55c21559ea8e6476f2deaae7f6d299642b91b92
RaKeerS <n.srikar22@gmail.com>
2021-11-03 01:26:59 +0530

    feat(greeting-template): added a new greeting card screen (no code), to work as a form for gathering data.

---
+++ src/app/components/greeting-card/greeting-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-greeting-card',
+  templateUrl: './greeting-card.component.html',
+  styleUrls: ['./greeting-card.component.css']
+})
+export class GreetingCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
