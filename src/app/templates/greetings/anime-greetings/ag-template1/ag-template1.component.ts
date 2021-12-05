d7383b54a402cf97078cd14cfb6ffa8ae7cae78e
RaKeerS <n.srikar22@gmail.com>
2021-12-05 23:56:42 +0530

    feat(greetings-template): working on new templates.

--- src/app/templates/greetings/anime-greetings/ag-template1/ag-template1.component.ts
+++ src/app/templates/greetings/anime-greetings/ag-template1/ag-template1.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Store } from '@ngxs/store';
+import { SelectTemplateDOMString } from 'src/app/actions/birthday-greetings-actions';
+import { GreetingData, ModalData } from 'src/app/types/modal-types';
 
 @Component({
   selector: 'app-ag-template1',
@@ -7,9 +10,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AgTemplate1Component implements OnInit {
 
-  constructor() { }
+  public customMessage: string;
+  public recipientName: string;
+  public senderName: string;
+
+  @ViewChild('greetingTemplate') greetingTemplate!: ElementRef;
+
+  constructor(public modalData: ModalData<GreetingData>, private store: Store) {
+    this.customMessage = this.modalData.inputData?.customMessage!;
+    this.recipientName = this.modalData.inputData?.recipientName!;
+    this.senderName = this.modalData.inputData?.senderName || '';
+   }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit() {
+    this.store.dispatch(new SelectTemplateDOMString(this.greetingTemplate.nativeElement.outerHTML));
   }
 
 }
