f36f8fb8febf4a3a51eb15b6ab0e43dea5334be4
Srikar Naidu <n.srikar22@gmail.com>
2022-05-08 19:07:09 +0530

    fix(greetings-builder): refactoring code and cleanup.

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/birthday-greetings'),
+      dir: require('path').join(__dirname, './coverage/greetings-builder'),
       subdir: '.',
       reporters: [
         { type: 'html' },
